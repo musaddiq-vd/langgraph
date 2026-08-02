a9e4d34ec4e80b88ffad51858c3e58575dda5080
musaddiq-vd <139722242+musaddiq-vd@users.noreply.github.com>
2026-08-02 22:38:02 +0530

    with bedrock & langgraph
    simple chatbot with bedrock and langgraph

---
+++ 01_simple_chatbot.py
@@ -0,0 +1,39 @@
+from langgraph.graph import StateGraph, START, END
+from langchain_aws import ChatBedrockConverse
+from typing import TypedDict
+from langchain_core.messages import HumanMessage, SystemMessage
+
+class AgentState(TypedDict):
+    messages: list
+
+llm = ChatBedrockConverse(
+    model="amazon.nova-pro-v1:0"
+)
+
+def chatbot(state: AgentState):
+    response = llm.invoke(state["messages"])
+    return {"messages":[response]}
+
+graph = StateGraph(AgentState)
+
+graph.add_node("chatbot", chatbot)
+
+graph.add_edge(START, "chatbot")
+graph.add_edge("chatbot",END)
+
+app = graph.compile()
+
+print("\t🤖 Welcome\nto exit type --> exit")
+while True:
+
+    user_input = input("👤 you: ")
+    if user_input.lower() == "exit":
+        break
+
+    response = app.invoke({
+        "messages": [
+            SystemMessage(content="you are helpful assistant, answer user questions carefully to the point"),
+            HumanMessage(content=user_input)
+        ]
+    })
+    print("\n🤖 AI: ",response["messages"][-1].content)
